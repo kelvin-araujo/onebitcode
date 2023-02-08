af1bcf16f6af6f5d9a9dace642f89c25eb562276
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-02-08 16:51:15 -0300

    created recrutas.html and recrutas.js

---
+++ JavaScript/Modulo I/exercicio-1/recrutas.js
@@ -0,0 +1,12 @@
+const nome = prompt('Informe o nome do recruta:')
+const sobrenome = prompt('Informe o sobrenome do recruta:')
+const estudo = prompt('Qual é o campo de estudo do recruta?')
+const ano = prompt('Qual o ano do nascimento do recruta?')
+const idade = 2023 - (ano)
+
+alert(
+  ' Cadastro realizado com sucesso! \n' +
+  '\n Nome completo:' + nome + '' + sobrenome + 
+  '\n Campo de estudo:' + estudo +
+  '\n Idade:' + idade 
+)
