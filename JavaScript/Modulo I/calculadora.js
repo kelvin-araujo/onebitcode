bd246563d01b263678694138dc460242cba29805
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-02-08 17:14:12 -0300

    created calculadora.html and calculadora.js

---
+++ JavaScript/Modulo I/calculadora.js
@@ -0,0 +1,16 @@
+const num1 = Number(prompt('Insira o primeiro número'))
+const num2 = Number(prompt('Insira o segundo número'))
+
+const soma = num1 + num2
+const subtracao = num1 - num2
+const multiplicacao = num1 * num2
+const divisao = num1 / num2
+
+alert (
+  'Resultados:\n' +
+  '\n Soma: ' + soma +
+  '\n Subtração ' + subtracao +
+  '\n Multiplicação: ' + multiplicacao +
+  '\n Divisão: ' + divisao
+)
+
