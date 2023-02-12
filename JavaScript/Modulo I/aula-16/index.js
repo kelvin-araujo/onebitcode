ac8962243925805dfdcb9c8308c6d2c4f553c188
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-02-12 16:45:09 -0300

    created aula-16

---
+++ JavaScript/Modulo I/aula-16/index.js
@@ -0,0 +1,17 @@
+/* Método if e else
+const idade = prompt('Informe a sua idade:')
+
+if(idade >= 18) {
+  alert('Você é maior de idade')
+} else if (idade >= 12) {
+  alert('Você é menor de idade')
+} else if (idade >= 4) {
+  alert('Você é criança')
+} else {
+  alert('')
+}*/
+
+// Método operador ternário
+
+const resultado = (6 === 5) ? 'Verdadeiro' : 'Falso'
+console.log(resultado)
