26df099c8b172eefd2a24e0024dd69bcb9d32d33
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-02-15 16:27:40 -0300

    Update dano.js

--- JavaScript/Modulo I/exercicio-2/dano.js
+++ JavaScript/Modulo I/exercicio-2/dano.js
@@ -1,5 +1,28 @@
 const attacker = prompt('Qual é o nome do personagem atacante?')
-const powerAttack = Number(prompt('Qual o poder do atacante? '))
+const powerAttack = prompt('Qual o poder do atacante? ')
 
-const defender  = prompt('Qual o nome do personagem de defesa')
-let life = Number(prompt('Quantos pontos de vida ele possui?'))
+const nameDefender  = prompt('Qual o nome do personagem de defesa?')
+let lifeDefender = prompt('Quantos pontos de vida ele possui?')
+const powerDefender = prompt('Qual é o seu poder de defesa?')
+const shield = prompt('Ele possui um escudo? (Sim/Não)')
+
+let demage = 0
+
+if (powerAttack > powerDefender && shield === 'Não') {
+  demage = powerAttack - powerDefender
+} else if (powerAttack > powerDefender && shield === 'Sim') {
+  demage =  (powerAttack - powerDefender) / 2
+} 
+
+lifeDefender -= demage
+
+alert(attacker + ' causou ' + demage + ' pontos de dano em ' + nameDefender + '.')
+alert(
+  attacker + '\n Poder de ataque: ' + powerAttack + '\n\n' +
+  nameDefender + '\n Pontos de vida: ' + lifeDefender +
+  '\n Poder de defesa: ' + powerDefender + '\n Possui escudo: ' + shield
+  )
+
+
+
+
