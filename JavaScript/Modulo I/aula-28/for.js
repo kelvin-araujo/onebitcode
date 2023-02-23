e83cc143c34a9288d9d6fc2483b1f0b4ef1806d6
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-02-23 16:57:03 -0300

    update

---
+++ JavaScript/Modulo I/aula-28/for.js
@@ -0,0 +1,11 @@
+// inicialização ; condição ; finalização
+let indice = 0;
+
+for (
+  alert('Inicializando o for'); 
+  indice <= 3; 
+  alert('Finalizando um bloco de repetição')
+  ) {
+  alert('Índice = ' + indice)
+  indice++
+}
