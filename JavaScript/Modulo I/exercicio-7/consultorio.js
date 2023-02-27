5083b5a74d922ea75e80acc211850b2f4d093db7
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-02-27 14:13:21 -0300

    created exercicio-7

---
+++ JavaScript/Modulo I/exercicio-7/consultorio.js
@@ -0,0 +1,34 @@
+let row = []
+let option = ''
+
+do {
+  let patients = ''
+  for (let i = 0; i < row; i++) {
+    patients += (i + 1) + 'º - ' + row[i] + '\n'
+  }
+
+  option = prompt(
+    'Pacientes:\n' + patients +
+    '\nEscolha uma ação:\n1. Novo Paciente\n2. Consultar Paciente\n3. Sair'
+  )
+
+  switch(opcao) {
+    case '1':
+      const newPatients = prompt('Qual é o nome do paciente?')
+      row.push(newPatients)
+      break
+    case '2':
+      const patientConsulted = row.shift()
+      if (row.length > 0) {
+        alert(patientConsulted + ' foi removido da fila.')
+      } else {
+        alert('Não há pacientes na fila!')
+      }
+      break
+    case '3':
+      alert('Encerrando...')
+      break
+      default:
+        alert('Opção inválida"')
+  }
+} while (option !== '3')
