0f6eb4c24a25c9d7093ca87fd8d04bc3cb0ce3a0
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-03-03 14:40:10 -0300

    Update parametros.js

--- JavaScript/Modulo I/aula-42/parametros.js
+++ JavaScript/Modulo I/aula-42/parametros.js
@@ -5,11 +5,15 @@ function dobro(x) {
 //dobro(5)
 //dobro(7)
 
+// ----------xx-------------
+
 function dizerOla(nome = 'mundo') {
   alert("Ola, " + nome + '!')
 }
 
 //dizerOla('Kelvin')
+
+// ----------xx-------------
 
 function soma(a, b) {
   alert('O resultado da soma é ' + (a + b))
@@ -17,6 +21,8 @@ function soma(a, b) {
 
 //soma(7, 6)
 //soma(1, 1)
+
+// ----------xx-------------
 
 function criarUsuario(nome, email, senha, tipo = 'admin') {
   const usuario = {
@@ -32,17 +38,21 @@ function criarUsuario(nome, email, senha, tipo = 'admin') {
 
 criarUsuario('Kelvin', 'kelvin@gmail.com', '1234')
 
+// ----------xx-------------
+
 function muitosParametros(nome, telefone, endereco, aniversario,email,senha) {
   //...
 }
+
+muitosParametros('nome', 'telefone', 'endereco', 'aniversario', 'email', 'senha')
+
+// ----------xx-------------
 
 function objComoParametro(usuario) {
   usuario.nome
   usuario.email
   
 }
-
-muitosParametros('nome', 'telefone', 'endereco', 'aniversario', 'email', 'senha')
 
 const dadosDoUsuario = {
   nome: '',
