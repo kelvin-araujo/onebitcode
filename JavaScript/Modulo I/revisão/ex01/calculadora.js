8944193589b6078795eea7b42d2dc1d32ea6d79a
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-03-20 15:47:41 -0300

    Update calculadora.js

--- JavaScript/Modulo I/revisão/ex01/calculadora.js
+++ JavaScript/Modulo I/revisão/ex01/calculadora.js
@@ -1,2 +1,15 @@
-let numberOne = Number(prompt("Insira o primeiro  número:"))
-let numberTwo = Number(prompt("Insira o segundo número:"))
+const numberOne = Number(prompt("Insira o primeiro  número:"))
+const numberTwo = Number(prompt("Insira o segundo número:"))
+
+const soma = numberOne + numberTwo
+const subtracao = numberOne - numberTwo
+const multiplicacao = numberOne * numberTwo
+const divisao = numberOne / numberTwo
+
+alert (
+  "Resultado das operações:" +
+  "\nSoma: " + soma +
+  "\nSubtração: " + subtracao +
+  "\nMultiplicação: " + multiplicacao +
+  "\nDivisão: " + divisao
+)
