f7893b6f371755150483eafeeaa571eba13aab6e
Kelvin Araújo <kelvinaraujoh@gmail.com>
2023-04-04 17:09:00 -0300

    Modulo02 aula-02

---
+++ JavaScript/Modulo II/aula-02/index.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+  console.log('Entrada do mouse')
+}
+
+function onChange() {
+  console.log('Mudança de valor!')
+}
+
+function onFocus() {
+  console.log('Foco no elemento')
+}
+
+function onBlur() {
+  console.log('Desfocado o elemento')
+}
+
+function onClick() {
+  console.log('Clicou no botão!')
+}
